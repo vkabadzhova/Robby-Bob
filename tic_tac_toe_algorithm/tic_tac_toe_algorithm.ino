62d78b0b11609f7bbfa819ac49b4191e02e0fff1
vili <vkabadzhova@gmail.com>
2021-04-10 21:05:45 +0300

    Make mirror Tic Tac Toe image

--- tic_tac_toe_algorithm/tic_tac_toe_algorithm.ino
+++ tic_tac_toe_algorithm/tic_tac_toe_algorithm.ino
@@ -17,23 +17,23 @@ bool playerLed = true;
 
 void setup() {
   randomSeed(analogRead(0));
-  
+
   for (int i = 0; i < ROWS_NUM; ++i) {
     for (int j = 0; j < COLS_NUM; ++j) {
       boardState[i][j] = 0;
     }
   }
-  
+
   Serial.begin(9600);
 }
 
-void loop() {  
+void loop() {
   int button = 0;
   do
   {
     button = GetButton();
   }
-  while(button == -1);
+  while (button == -1);
 
   int row = button / 3;
   int col = button % 3;
@@ -42,16 +42,16 @@ void loop() {
 
   DrawTicTacToe();
   delay(1000);
-      
+
   GetButton();
   delay(200);
-      
-  if(HasWinner())
+
+  if (HasWinner())
   {
     FlashWinner(1);
     ResetGame();
   }
-  else if (totalGameMoves == 5) 
+  else if (totalGameMoves == 5)
   {
     FlashEqual();
     ResetGame();
@@ -62,26 +62,26 @@ void loop() {
     delay(1000);
     DrawTicTacToe();
     delay(1000);
-    
-    if(HasWinner())
+
+    if (HasWinner())
     {
       FlashWinner(2);
       ResetGame();
     }
   }
-  
+
   delay(200);
-  
+
 }
 
 void ComputerMove()
 {
   bool done = false;
   delay(1000);
-  if(totalGameMoves == 1)
+  if (totalGameMoves == 1)
   {
     //if first player move isn't the center, take the center
-    if(boardState[1][1] == 0)
+    if (boardState[1][1] == 0)
     {
       boardState[1][1] = 2;
     }
@@ -89,36 +89,36 @@ void ComputerMove()
     {
       int row = random(0, 2) << 1;
       int col = random(0, 2) << 1;
-      
+
       boardState[row][col] = 2;
     }
   }
-  
-  if(totalGameMoves == 2)
+
+  if (totalGameMoves == 2)
   {
     done = false;
     // if player has 2 in a row, block it
     done = BlockPlayerWin();
-    
+
     // look for any doubling traps.
-    if(!done)
+    if (!done)
     {
       // Corner-opposite corner.  Take any side.  Corner would lead to loss.
       if (((boardState[0][0] == PLAYER_BOARD_MARK) && (boardState[2][2] == PLAYER_BOARD_MARK)) ||
           ((boardState[0][2] == PLAYER_BOARD_MARK) && (boardState[2][0] == PLAYER_BOARD_MARK)))
       {
-        int row = random(0,3);
+        int row = random(0, 3);
         int col = 1;
-        if(row == 1) col = random(0,2) << 1;
+        if (row == 1) col = random(0, 2) << 1;
         boardState[row][col] = 2;
         done = true;
       }
-      
+
       // Side-corner. Take inside corner.
       //top-center plus corner
-      if(!done && (boardState[0][1] == PLAYER_BOARD_MARK))
+      if (!done && (boardState[0][1] == PLAYER_BOARD_MARK))
       {
-        if(boardState[2][0] == PLAYER_BOARD_MARK)
+        if (boardState[2][0] == PLAYER_BOARD_MARK)
         {
           boardState[0][0] = 2;
           done = true;
@@ -129,11 +129,11 @@ void ComputerMove()
           done = true;
         }
       }
-      
+
       //bottom -center plus corner
-      if(!done && (boardState[2][1] == PLAYER_BOARD_MARK))
+      if (!done && (boardState[2][1] == PLAYER_BOARD_MARK))
       {
-        if(boardState[0][0] == PLAYER_BOARD_MARK)
+        if (boardState[0][0] == PLAYER_BOARD_MARK)
         {
           boardState[2][0] = 2;
           done = true;
@@ -144,11 +144,11 @@ void ComputerMove()
           done = true;
         }
       }
-      
+
       //center-left plus corner
-      if(!done && (boardState[1][0] == PLAYER_BOARD_MARK))
+      if (!done && (boardState[1][0] == PLAYER_BOARD_MARK))
       {
-        if(boardState[0][2] == PLAYER_BOARD_MARK)
+        if (boardState[0][2] == PLAYER_BOARD_MARK)
         {
           boardState[0][0] = 2;
           done = true;
@@ -159,11 +159,11 @@ void ComputerMove()
           done = true;
         }
       }
-      
+
       //center-right plus corner
-      if(!done && (boardState[1][2] == PLAYER_BOARD_MARK))
+      if (!done && (boardState[1][2] == PLAYER_BOARD_MARK))
       {
-        if(boardState[0][0] == PLAYER_BOARD_MARK)
+        if (boardState[0][0] == PLAYER_BOARD_MARK)
         {
           boardState[0][2] = 2;
           done = true;
@@ -174,23 +174,23 @@ void ComputerMove()
           done = true;
         }
       }
-      
+
       // Center-corner (if computer has opposite corner).  Take either remaining corner.
-      if(!done)
+      if (!done)
       {
-        if(boardState[1][1] == PLAYER_BOARD_MARK)
+        if (boardState[1][1] == PLAYER_BOARD_MARK)
         {
-          if(((boardState[0][0] == 2) && (boardState[2][2] == PLAYER_BOARD_MARK)) || ((boardState[0][0] == PLAYER_BOARD_MARK) && (boardState[2][2] == 2)))
+          if (((boardState[0][0] == 2) && (boardState[2][2] == PLAYER_BOARD_MARK)) || ((boardState[0][0] == PLAYER_BOARD_MARK) && (boardState[2][2] == 2)))
           {
-            if(random(0,2) == 1)
+            if (random(0, 2) == 1)
               boardState[0][2] = 2;
             else
               boardState[2][0] = 2;
             done = true;
           }
-          else if(((boardState[0][2] == 2) && (boardState[2][0] == PLAYER_BOARD_MARK)) || ((boardState[0][2] == PLAYER_BOARD_MARK) && (boardState[2][0] == 2)))
+          else if (((boardState[0][2] == 2) && (boardState[2][0] == PLAYER_BOARD_MARK)) || ((boardState[0][2] == PLAYER_BOARD_MARK) && (boardState[2][0] == 2)))
           {
-            if(random(0,2) == 1)
+            if (random(0, 2) == 1)
               boardState[0][0] = 2;
             else
               boardState[2][2] = 2;
@@ -198,7 +198,7 @@ void ComputerMove()
           }
         }
       }
-    
+
       // Otherwise take any random spot
       if (!done)
       {
@@ -209,26 +209,26 @@ void ComputerMove()
           row = random(0, 3);
           col = random(0, 3);
         }
-        while(boardState[row][col] > 0);
-      
+        while (boardState[row][col] > 0);
+
         boardState[row][col] = 2;
       }
     }
   }
-  
-  if(totalGameMoves >= 3)
+
+  if (totalGameMoves >= 3)
   {
     done = false;
     //if we have 2 in a row and can get 3 to win, do it
     done = FindComputerWin();
-    
+
     //if player has 2 in a row, block it
-    if(!done) done = BlockPlayerWin();
-    
+    if (!done) done = BlockPlayerWin();
+
     //else todo?
 
     // take any random spot
-    if(!done)
+    if (!done)
     {
       int row = 1;
       int col = 1;
@@ -237,8 +237,8 @@ void ComputerMove()
         row = random(0, 3);
         col = random(0, 3);
       }
-      while(boardState[row][col] > 0);
-      
+      while (boardState[row][col] > 0);
+
       boardState[row][col] = 2;
     }
   }
@@ -247,51 +247,51 @@ void ComputerMove()
 bool FindComputerWin()
 {
   bool done = false;
-  
+
   //down diag
-  if((boardState[0][0] == 2) && (boardState[1][1] == 2) && (boardState[2][2] == 0))
+  if ((boardState[0][0] == 2) && (boardState[1][1] == 2) && (boardState[2][2] == 0))
   {
     boardState[2][2] = 2;
     done = true;
   }
-  else if((boardState[2][2] == 2) && (boardState[1][1] == 2) && (boardState[0][0] == 0))
+  else if ((boardState[2][2] == 2) && (boardState[1][1] == 2) && (boardState[0][0] == 0))
   {
     boardState[0][0] = 2;
     done = true;
   }
-  
+
   //up diag
-  if(!done)
+  if (!done)
   {
-    if((boardState[0][2] == 2) && (boardState[1][1] == 2) && (boardState[2][0] == 0))
+    if ((boardState[0][2] == 2) && (boardState[1][1] == 2) && (boardState[2][0] == 0))
     {
       boardState[2][0] = 2;
       done = true;
     }
-    else if((boardState[2][0] == 2) && (boardState[1][1] == 2) && (boardState[0][2] == 0))
+    else if ((boardState[2][0] == 2) && (boardState[1][1] == 2) && (boardState[0][2] == 0))
     {
       boardState[0][2] = 2;
       done = true;
     }
   }
   if (done) return done;
-  
+
   //rows
-  for(int row = 0; row < 3; row++)
+  for (int row = 0; row < 3; row++)
   {
-    if((boardState[row][0] == 2) && (boardState[row][1] == 2) && (boardState[row][2] == 0))
+    if ((boardState[row][0] == 2) && (boardState[row][1] == 2) && (boardState[row][2] == 0))
     {
       boardState[row][2] = 2;
       done = true;
       break;
     }
-    else if((boardState[row][0] == 2) && (boardState[row][1] == 0) && (boardState[row][2] == 2))
+    else if ((boardState[row][0] == 2) && (boardState[row][1] == 0) && (boardState[row][2] == 2))
     {
       boardState[row][1] = 2;
       done = true;
       break;
     }
-    else if((boardState[row][0] == 0) && (boardState[row][1] == 2) && (boardState[row][2] == 2))
+    else if ((boardState[row][0] == 0) && (boardState[row][1] == 2) && (boardState[row][2] == 2))
     {
       boardState[row][0] = 2;
       done = true;
@@ -301,21 +301,21 @@ bool FindComputerWin()
   if (done) return done;
 
   //cols
-  for(int col = 0; col < 3; col++)
+  for (int col = 0; col < 3; col++)
   {
-    if((boardState[0][col] == 2) && (boardState[1][col] == 2) && (boardState[2][col] == 0))
+    if ((boardState[0][col] == 2) && (boardState[1][col] == 2) && (boardState[2][col] == 0))
     {
       boardState[2][col] = 2;
       done = true;
       break;
     }
-    else if((boardState[0][col] == 2) && (boardState[1][col] == 0) && (boardState[2][col] == 2))
+    else if ((boardState[0][col] == 2) && (boardState[1][col] == 0) && (boardState[2][col] == 2))
     {
       boardState[1][col] = 2;
       done = true;
       break;
     }
-    else if((boardState[0][col] == 0) && (boardState[1][col] == 2) && (boardState[2][col] == 2))
+    else if ((boardState[0][col] == 0) && (boardState[1][col] == 2) && (boardState[2][col] == 2))
     {
       boardState[0][col] = 2;
       done = true;
@@ -328,51 +328,51 @@ bool FindComputerWin()
 bool BlockPlayerWin()
 {
   bool done = false;
-  
+
   //down diag
-  if((boardState[0][0] == PLAYER_BOARD_MARK) && (boardState[1][1] == PLAYER_BOARD_MARK) && (boardState[2][2] == 0))
+  if ((boardState[0][0] == PLAYER_BOARD_MARK) && (boardState[1][1] == PLAYER_BOARD_MARK) && (boardState[2][2] == 0))
   {
     boardState[2][2] = 2;
     done = true;
   }
-  else if((boardState[2][2] == PLAYER_BOARD_MARK) && (boardState[1][1] == PLAYER_BOARD_MARK) && (boardState[0][0] == 0))
+  else if ((boardState[2][2] == PLAYER_BOARD_MARK) && (boardState[1][1] == PLAYER_BOARD_MARK) && (boardState[0][0] == 0))
   {
     boardState[0][0] = 2;
     done = true;
   }
-  
+
   //up diag
-  if(!done)
+  if (!done)
   {
-    if((boardState[0][2] == PLAYER_BOARD_MARK) && (boardState[1][1] == PLAYER_BOARD_MARK) && (boardState[2][0] == 0))
+    if ((boardState[0][2] == PLAYER_BOARD_MARK) && (boardState[1][1] == PLAYER_BOARD_MARK) && (boardState[2][0] == 0))
     {
       boardState[2][0] = 2;
       done = true;
     }
-    else if((boardState[2][0] == PLAYER_BOARD_MARK) && (boardState[1][1] == PLAYER_BOARD_MARK) && (boardState[0][2] == 0))
+    else if ((boardState[2][0] == PLAYER_BOARD_MARK) && (boardState[1][1] == PLAYER_BOARD_MARK) && (boardState[0][2] == 0))
     {
       boardState[0][2] = 2;
       done = true;
     }
   }
   if (done) return done;
-  
+
   //rows
-  for(int row = 0; row < 3; row++)
+  for (int row = 0; row < 3; row++)
   {
-    if((boardState[row][0] == PLAYER_BOARD_MARK) && (boardState[row][1] == PLAYER_BOARD_MARK) && (boardState[row][2] == 0))
+    if ((boardState[row][0] == PLAYER_BOARD_MARK) && (boardState[row][1] == PLAYER_BOARD_MARK) && (boardState[row][2] == 0))
     {
       boardState[row][2] = 2;
       done = true;
       break;
     }
-    else if((boardState[row][0] == PLAYER_BOARD_MARK) && (boardState[row][1] == 0) && (boardState[row][2] == PLAYER_BOARD_MARK))
+    else if ((boardState[row][0] == PLAYER_BOARD_MARK) && (boardState[row][1] == 0) && (boardState[row][2] == PLAYER_BOARD_MARK))
     {
       boardState[row][1] = 2;
       done = true;
       break;
     }
-    else if((boardState[row][0] == 0) && (boardState[row][1] == PLAYER_BOARD_MARK) && (boardState[row][2] == PLAYER_BOARD_MARK))
+    else if ((boardState[row][0] == 0) && (boardState[row][1] == PLAYER_BOARD_MARK) && (boardState[row][2] == PLAYER_BOARD_MARK))
     {
       boardState[row][0] = 2;
       done = true;
@@ -382,21 +382,21 @@ bool BlockPlayerWin()
   if (done) return done;
 
   //cols
-  for(int col = 0; col < 3; col++)
+  for (int col = 0; col < 3; col++)
   {
-    if((boardState[0][col] == PLAYER_BOARD_MARK) && (boardState[1][col] == PLAYER_BOARD_MARK) && (boardState[2][col] == 0))
+    if ((boardState[0][col] == PLAYER_BOARD_MARK) && (boardState[1][col] == PLAYER_BOARD_MARK) && (boardState[2][col] == 0))
     {
       boardState[2][col] = 2;
       done = true;
       break;
     }
-    else if((boardState[0][col] == PLAYER_BOARD_MARK) && (boardState[1][col] == 0) && (boardState[2][col] == PLAYER_BOARD_MARK))
+    else if ((boardState[0][col] == PLAYER_BOARD_MARK) && (boardState[1][col] == 0) && (boardState[2][col] == PLAYER_BOARD_MARK))
     {
       boardState[1][col] = 2;
       done = true;
       break;
     }
-    else if((boardState[0][col] == 0) && (boardState[1][col] == PLAYER_BOARD_MARK) && (boardState[2][col] == PLAYER_BOARD_MARK))
+    else if ((boardState[0][col] == 0) && (boardState[1][col] == PLAYER_BOARD_MARK) && (boardState[2][col] == PLAYER_BOARD_MARK))
     {
       boardState[0][col] = 2;
       done = true;
@@ -409,39 +409,39 @@ bool BlockPlayerWin()
 
 bool HasWinner()
 {
-  for(int player = 1; player < 3; player++)
+  for (int player = 1; player < 3; player++)
   {
-    if(boardState[0][0] == player)
+    if (boardState[0][0] == player)
     {
-      if((boardState[1][1] == player) && (boardState[2][2] == player)) return true; // right-down diag
-      if((boardState[1][0] == player) && (boardState[2][0] == player)) return true; //col 1
-      if((boardState[0][1] == player) && (boardState[0][2] == player)) return true; // row 1
+      if ((boardState[1][1] == player) && (boardState[2][2] == player)) return true; // right-down diag
+      if ((boardState[1][0] == player) && (boardState[2][0] == player)) return true; //col 1
+      if ((boardState[0][1] == player) && (boardState[0][2] == player)) return true; // row 1
     }
-    if((boardState[1][0] == player) && (boardState[1][1] == player) && (boardState[1][2] == player)) return true; //row 2
-    if((boardState[2][0] == player) && (boardState[2][1] == player) && (boardState[2][2] == player)) return true; //row 3
-    if((boardState[0][1] == player) && (boardState[1][1] == player) && (boardState[2][1] == player)) return true; //col 2
-    if((boardState[0][2] == player) && (boardState[1][2] == player) && (boardState[2][2] == player)) return true; //col 3
+    if ((boardState[1][0] == player) && (boardState[1][1] == player) && (boardState[1][2] == player)) return true; //row 2
+    if ((boardState[2][0] == player) && (boardState[2][1] == player) && (boardState[2][2] == player)) return true; //row 3
+    if ((boardState[0][1] == player) && (boardState[1][1] == player) && (boardState[2][1] == player)) return true; //col 2
+    if ((boardState[0][2] == player) && (boardState[1][2] == player) && (boardState[2][2] == player)) return true; //col 3
 
-    if((boardState[2][0] == player) && (boardState[1][1] == player) && (boardState[0][2] == player)) return true; //right-up diag
+    if ((boardState[2][0] == player) && (boardState[1][1] == player) && (boardState[0][2] == player)) return true; //right-up diag
   }
   return false;
 }
 
 void FlashEqual() {
-  for(int i = 0; i < 5; ++i)
+  for (int i = 0; i < 5; ++i)
   {
     Serial.println("No winner - you're equal with Robob!");
-  }  
+  }
 }
 
 void FlashWinner(int playerOrComp)
 {
   String who = (playerOrComp == PLAYER_BOARD_MARK) ? "PLAYER" : "ROBOB";
-  for(int i = 0; i < 10; ++i)
+  for (int i = 0; i < 10; ++i)
   {
     Serial.print(who);
     Serial.println(" won!");
-  }  
+  }
 }
 
 void ResetGame()
@@ -449,21 +449,21 @@ void ResetGame()
   delay(500);
   totalGameMoves = 0;
   //FlashWinner erases the boardState array.  But there may have been a tie.  So erase anyway.
-  for(int row = 0; row < 3; row++)
-      for(int col = 0; col < 3; col++)
-        boardState[row][col] = 0;
+  for (int row = 0; row < 3; row++)
+    for (int col = 0; col < 3; col++)
+      boardState[row][col] = 0;
   Serial.println("\n\n\nGAME RESETED! Now it's your turn.");
 }
 
 bool UserInputValid(int button)
 {
   int result = false;
-  
+
   button--;
   int row = button / 3;
   int col = button % 3;
-  
-  if(boardState[row][col] == 0) result = true;
+
+  if (boardState[row][col] == 0) result = true;
 
   return result;
 }
@@ -476,26 +476,26 @@ int GetButton()
   int btnRow3 = analogRead(A2);
 
   const int buffer_range = 20;
-  if (btnRow1 >= MAGIC_NUM_LEFT && btnRow1 <= MAGIC_NUM_LEFT + buffer_range) return 0;
-  if (btnRow1 >= MAGIC_NUM_MIDDLE && btnRow1 <= MAGIC_NUM_MIDDLE + buffer_range) return 1;
-  if (btnRow1 >= MAGIC_NUM_RIGHT && btnRow1 <= MAGIC_NUM_RIGHT + buffer_range) return 2;
-  
-  if (btnRow2 >= MAGIC_NUM_LEFT && btnRow2 <= MAGIC_NUM_LEFT + buffer_range) return 3;
-  if (btnRow2 >= MAGIC_NUM_MIDDLE && btnRow2 <= MAGIC_NUM_MIDDLE + buffer_range) return 4;
-  if (btnRow2 >= MAGIC_NUM_RIGHT && btnRow2 <= MAGIC_NUM_RIGHT + buffer_range) return 5;
-  
-  if (btnRow3 >= MAGIC_NUM_LEFT && btnRow3 <= MAGIC_NUM_LEFT + buffer_range) return 6;
-  if (btnRow3 >= MAGIC_NUM_MIDDLE && btnRow3 <= MAGIC_NUM_MIDDLE + buffer_range) return 7;
-  if (btnRow3 >= MAGIC_NUM_RIGHT && btnRow3 <= MAGIC_NUM_RIGHT + buffer_range) return 8;
 
-    /*Serial.print("btnRow1 :");
+  if (btnRow1 >= MAGIC_NUM_LEFT && btnRow1 <= MAGIC_NUM_LEFT + buffer_range) return 8;
+  if (btnRow1 >= MAGIC_NUM_MIDDLE && btnRow1 <= MAGIC_NUM_MIDDLE + buffer_range) return 7;
+  if (btnRow1 >= MAGIC_NUM_RIGHT && btnRow1 <= MAGIC_NUM_RIGHT + buffer_range) return 6;
+
+  if (btnRow2 >= MAGIC_NUM_LEFT && btnRow2 <= MAGIC_NUM_LEFT + buffer_range) return 5;
+  if (btnRow2 >= MAGIC_NUM_MIDDLE && btnRow2 <= MAGIC_NUM_MIDDLE + buffer_range) return 4;
+  if (btnRow2 >= MAGIC_NUM_RIGHT && btnRow2 <= MAGIC_NUM_RIGHT + buffer_range) return 3;
+
+  if (btnRow3 >= MAGIC_NUM_LEFT && btnRow3 <= MAGIC_NUM_LEFT + buffer_range) return 2;
+  if (btnRow3 >= MAGIC_NUM_MIDDLE && btnRow3 <= MAGIC_NUM_MIDDLE + buffer_range) return 1;
+  if (btnRow3 >= MAGIC_NUM_RIGHT && btnRow3 <= MAGIC_NUM_RIGHT + buffer_range) return 0;
+  /*Serial.print("btnRow1 :");
     Serial.println(btnRow1);
     Serial.print("btnRow2 :");
     Serial.println(btnRow2);
     Serial.print("btnRow3 :");
     Serial.println(btnRow3);
     delay(500);*/
-  
+
   return -1;
 }
 
@@ -503,21 +503,27 @@ void DrawTicTacToe() {
   for (short i = 0; i < 20; ++i) {
     Serial.println();
   }
-  
+
   for (int i = 0; i < ROWS_NUM; ++i) {
     Serial.print("| ");
     String current_player_sign = " ";
     for (int j = 0; j < COLS_NUM; ++j) {
-      if (boardState[i][j] == PLAYER_BOARD_MARK) {Serial.print("X | ");}
-      else if (boardState[i][j] == 2) {Serial.print("O | ");}
-      else {Serial.print("  | ");}
-     
+      if (boardState[i][j] == PLAYER_BOARD_MARK) {
+        Serial.print("X | ");
+      }
+      else if (boardState[i][j] == 2) {
+        Serial.print("O | ");
+      }
+      else {
+        Serial.print("  | ");
+      }
+
     }
-    
+
     Serial.println("\n");
   }
-  
+
   for (int i = 0; i < 3; ++i) {
-    Serial.println();  
+    Serial.println();
   }
 }
