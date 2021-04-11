cf51eeea4a1aac4d9a407e527994dbd7b4dcfcf3
vili <vkabadzhova@gmail.com>
2021-04-11 15:17:50 +0300

    Merge Robob and the tic tac toe program

--- robob_movement/robob_movement.ino
+++ robob_movement/robob_movement.ino
@@ -37,6 +37,7 @@ void move_to_pos_1() {
   const int down_servo_angle = 90;
   const int middle_servo_angle = 29;
   const int upper_servo_angle = 60;
+  
   write_angles(down_servo_angle, middle_servo_angle, upper_servo_angle);
 }
 
@@ -102,6 +103,11 @@ void move_to_home_pos() {
   move_to_pos_0();
 }
 
+const void (*positions[])() = {move_to_home_pos, move_to_pos_1, move_to_pos_2,
+                               move_to_pos_3, move_to_pos_4,  move_to_pos_5, 
+                               move_to_pos_6,  move_to_pos_7,  move_to_pos_8, move_to_pos_9
+                              };
+
 void setup() {
   DownServo.attach(S1_PIN);
   MiddleServo.attach(S2_PIN);
@@ -112,10 +118,10 @@ void setup() {
   Serial.begin(9600);
 }
 
-
 void loop() {
   delay(BTW_POS_DELAY);
-  move_to_pos_9();
+  positions[0]();
+  //move_to_pos_9();
 
   delay(BTW_POS_DELAY);
   move_to_pos_8();
