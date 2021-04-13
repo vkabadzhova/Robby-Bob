73474966bae16b5805588c64aa9a1bb34aa9c847
vili <vkabadzhova@gmail.com>
2021-04-13 18:48:06 +0300

    Refix positions

--- robob_movement/robob_movement.ino
+++ robob_movement/robob_movement.ino
@@ -33,67 +33,67 @@ void move_to_pos_0() {
   write_angles(down_servo_angle, middle_servo_angle, upper_servo_angle);
 }
 
-void move_to_pos_1() {
-  const int down_servo_angle = 90;
-  const int middle_servo_angle = 29;
+void move_to_pos_9() {
+  const int down_servo_angle = 87;
+  const int middle_servo_angle = 28;
   const int upper_servo_angle = 60;
   
   write_angles(down_servo_angle, middle_servo_angle, upper_servo_angle);
 }
 
-void move_to_pos_2() {
-  const int down_servo_angle = 75;
-  const int middle_servo_angle = 30;
+void move_to_pos_8() {
+  const int down_servo_angle = 70;
+  const int middle_servo_angle = 28;
   const int upper_servo_angle = 60;
   write_angles(down_servo_angle, middle_servo_angle, upper_servo_angle);
 }
 
-void move_to_pos_3() {
-  const int down_servo_angle = 59;
-  const int middle_servo_angle = 29;
-  const int upper_servo_angle = 68;
+void move_to_pos_7() {
+  const int down_servo_angle = 55;
+  const int middle_servo_angle = 28;
+  const int upper_servo_angle = 66;
   write_angles(down_servo_angle, middle_servo_angle, upper_servo_angle);
 }
 
-void move_to_pos_4() {
-  const int down_servo_angle = 98;
+void move_to_pos_6() {
+  const int down_servo_angle = 96;
   const int middle_servo_angle = 29;
   const int upper_servo_angle = 40;
   write_angles(down_servo_angle, middle_servo_angle, upper_servo_angle);
 }
 
 void move_to_pos_5() {
-  const int down_servo_angle = 75;
+  const int down_servo_angle = 73;
   const int middle_servo_angle = 30;
-  const int upper_servo_angle = 40;
+  const int upper_servo_angle = 37;
   write_angles(down_servo_angle, middle_servo_angle, upper_servo_angle);
 }
 
-void move_to_pos_6() {
+void move_to_pos_4() {
   const int down_servo_angle = 51;
   const int middle_servo_angle = 29;
-  const int upper_servo_angle = 44;
+  const int upper_servo_angle = 42;
   write_angles(down_servo_angle, middle_servo_angle, upper_servo_angle);
 }
 
-void move_to_pos_7() {
+void move_to_pos_3() {
   const int down_servo_angle = 110;
   const int middle_servo_angle = 29;
-  const int upper_servo_angle = 24;
+  const int upper_servo_angle = 23;
   write_angles(down_servo_angle, middle_servo_angle, upper_servo_angle);
 }
 
-void move_to_pos_8() {
+void move_to_pos_2() {
   const int down_servo_angle = 80;
   const int middle_servo_angle = 29;
-  const int upper_servo_angle = 24;
+  const int upper_servo_angle = 21;
   write_angles(down_servo_angle, middle_servo_angle, upper_servo_angle);
 }
 
-void move_to_pos_9() {
+void move_to_pos_1() {
   const int down_servo_angle = 40;
   const int middle_servo_angle = 29;
-  const int upper_servo_angle = 28;
+  const int upper_servo_angle = 27;
   write_angles(down_servo_angle, middle_servo_angle, upper_servo_angle);
 }
 
@@ -119,40 +119,40 @@ void setup() {
 }
 
 void loop() {
-  delay(BTW_POS_DELAY);
-  positions[0]();
-  //move_to_pos_9();
-
-  delay(BTW_POS_DELAY);
-  move_to_pos_8();
-
-  delay(BTW_POS_DELAY);
-  move_to_pos_7();
-
-  delay(BTW_POS_DELAY);
-  move_to_home_pos();
-
-  delay(BTW_POS_DELAY);
-  move_to_pos_6();
-
-  delay(BTW_POS_DELAY);
-  move_to_pos_5();
-
+  
+//  delay(BTW_POS_DELAY);
+//  move_to_pos_9();
+//
+//  delay(BTW_POS_DELAY);
+//  move_to_pos_8();
+//
+//  delay(BTW_POS_DELAY);
+//  move_to_pos_7();
+//
+//  delay(BTW_POS_DELAY);
+//  move_to_home_pos();
+//
+//  delay(BTW_POS_DELAY);
+// move_to_pos_6();
+//
+//  delay(BTW_POS_DELAY);
+//  move_to_pos_5();
+//
   delay(BTW_POS_DELAY);
   move_to_pos_4();
-
-  delay(BTW_POS_DELAY);
-  move_to_home_pos();
-
-  delay(BTW_POS_DELAY);
-  move_to_pos_3();
-
-  delay(BTW_POS_DELAY);
-  move_to_pos_2();
-
-  delay(BTW_POS_DELAY);
-  move_to_pos_1();
-
-  delay(BTW_POS_DELAY);
-  move_to_home_pos();
+//
+//  delay(BTW_POS_DELAY);
+//  move_to_home_pos();
+//
+//  delay(BTW_POS_DELAY);
+//  move_to_pos_3();
+//
+//  delay(BTW_POS_DELAY);
+//  move_to_pos_2();
+//
+//  delay(BTW_POS_DELAY);
+//  move_to_pos_1();
+//
+//  delay(BTW_POS_DELAY);
+//  move_to_home_pos();
 }
